621f8ce67aa4905af32c0f2d8f9273f6baea15ef
daniel-hgames <daniel@harmonygames.co>
2024-11-07 01:47:33 -0300

    adding scriptable object for features

---
+++ Assets/Scripts/Utils/FeatureSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Feature Settings", menuName = "ScriptableObjects/Feature Settings", order = 1)]
+public class FeatureSettings : ScriptableObject
+{
+    public bool CollisionsEnabled;
+    public bool SkinSelectEnabled;
+    public bool DailyRewardsEnabled;
+    public bool CustomFeatureEnabled;
+}
