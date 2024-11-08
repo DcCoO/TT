5dce7b59c2dc1c69c4a63ec1d184229e11eef788
daniel-hgames <daniel@harmonygames.co>
2024-11-08 02:45:00 -0300

    adding debug menu ready

---
+++ Assets/Scripts/Utils/DebugRow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugRow : MonoBehaviour
+{
+    [SerializeField] private Text m_Title;
+    [SerializeField] private Toggle m_Toggle;
+
+    private Feature m_Feature;
+
+    public void Setup(Feature feature)
+    {
+        m_Feature = feature;
+        m_Title.text = feature.name;
+        m_Toggle.isOn = feature;
+
+        m_Toggle.onValueChanged.AddListener(OnValueChanged);
+    }
+
+    private void OnValueChanged(bool value)
+    {
+        m_Feature.IsEnabled = value;
+    }
+
+    private void OnDestroy()
+    {
+        m_Toggle.onValueChanged.RemoveListener(OnValueChanged);        
+    }
+}
---
+++ Assets/Scripts/Utils/DebugView.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class DebugView : MonoBehaviour
+{
+    [SerializeField] private GameObject m_MainPanel;
+    [SerializeField] private GameObject m_Overlay;
+
+    [SerializeField] private Feature[] m_Features;
+    [SerializeField] private DebugRow m_RowPrefab;
+
+    public void Show()
+    {
+        if (m_MainPanel.activeSelf) return;
+        if (GameManager.Instance.currentPhase != GamePhase.MAIN_MENU) return;
+
+        m_MainPanel.SetActive(true);
+        m_Overlay.SetActive(true);
+        foreach (var feature in m_Features)
+        {
+            var row = Instantiate(m_RowPrefab, m_MainPanel.transform);
+            row.Setup(feature);
+        }
+    }
+
+    public void Hide()
+    {
+        foreach (Transform child in m_MainPanel.transform)
+        {
+            if (child.gameObject.GetComponent<DebugRow>() != null)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+        m_MainPanel.SetActive(false);
+        m_Overlay.SetActive(false);
+    }
+}
