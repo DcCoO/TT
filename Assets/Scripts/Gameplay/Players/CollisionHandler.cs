63e6d7e78e12391448806972356dd3ade794e4d3
Luiz Thiago <luiz@argus.gg>
2024-11-08 20:38:53 -0500

    Added collision particles

---
+++ Assets/Scripts/Gameplay/Players/CollisionHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private ParticleSystem _particleSystem;
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.TryGetComponent(out CollisionHandler _))
+        {
+            _particleSystem.Play();
+        }
+    }
+}
